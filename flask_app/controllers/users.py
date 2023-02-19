407d3bece476757c7703bf2cc3978051cc1fb51a
Brendan Cordova <cordovalegacy19@gmail.com>
2023-02-19 07:19:33 -0800

    added validations

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,5 +1,6 @@
 from flask_app import app
-from flask import render_template, redirect
+from flask import render_template, redirect, request
+from flask_app.models.user import User
 
 @app.route('/')
 def home():
@@ -7,4 +8,13 @@ def home():
 
 @app.route('/register')
 def register():
+    if not User.validate(request.form):
+        return redirect('/')
+    form_data = {
+        'first_name': request.form['first_name'], 
+        'last_name': request.form['last_name'], 
+        'email': request.form['email'], 
+        'password': request.form['password'], 
+    }
+    User.save_registration(form_data)
     return redirect('/')
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -10,16 +10,41 @@ class User:
         self.first_name = data['first_name']
         self.last_name = data['last_name']
         self.email = data['email']
+        self.password = data['password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        results = connectToMySQL('users_schema').query_db(query)
+        results = connectToMySQL('log_reg').query_db(query)
         users = []
         for user in results:
             users.append(cls(user))
         return users
+
+    @classmethod
+    def save_registration(cls, form_data):
+        query = """
+                INSERT INTO login_registration (first_name, last_name, email, password, created_at, updated_at)
+                VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW(), NOW())
+                ;"""
+        return connectToMySQL('log_reg').query_db(query, form_data)
+    
+    @staticmethod
+    def validate(form_data):
+        is_valid = True
+        if len(form_data['first_name']) < 2:
+            flash("First Name must be at least 2 characters")
+            is_valid = False
+        if len(form_data['last_name']) < 2:
+            flash("Last Name must be at least 2 characters")
+            is_valid = False
+        if len(form_data['email']) < 12:
+            flash("Email Address must be at least 12 characters")
+            is_valid = False
+        if len(form_data['password']) < 2:
+            flash("Password must be at least 2 characters")
+            is_valid = False
 
 
